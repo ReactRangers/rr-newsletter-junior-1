6f2dc7dec9900204430628208963b96d68fc6e8e
Ewelina Wiechowicz <ewelina.wiechowicz@gmail.com>
2018-07-02 19:17:50 +0200

    Ewelina Wiechowicz - first task

--- src/CarsDataTable.js
+++ src/CarsDataTable.js
@@ -1,12 +1,50 @@
 import React from 'react';
 
-const CarsDataTable = (props) => {
-  return (
-    <div>
-      The list has {props.cars.length} cars.<br />
-      <b>Your task is to render a table here</b>.
-    </div>
-  );
-};
+type Props = {
+  cars: Array<Car>
+}
 
-export default CarsDataTable;
+type Car = {
+  id: string,
+  carMake: string,
+  carModel: string,
+  carModelYear: string,
+  bodyColor: string,
+  dealerEmail: string,
+}
+
+export default class CarsDataTable extends React.Component<Props> {
+  renderRow(car) {
+    return (
+      <tr>
+        <td>
+          {car.carMake}
+        </td>
+        <td>
+          {car.carModel}
+        </td>
+        <td>
+          {car.carModelYear}
+        </td>
+        <td style={{backgroundColor: car.bodyColor}} />
+        <td>
+          {car.dealerEmail}
+        </td>
+      </tr>
+    )
+  }
+  render() {
+    return (
+      <table>
+        <tr>
+          <th>Brand</th>
+          <th>Model number</th>
+          <th>Year</th>
+          <th>Color</th>
+          <th>Dealer email address</th>
+        </tr>
+        {this.props.cars.map(car => this.renderRow(car))}
+      </table>
+    )
+  }
+};
