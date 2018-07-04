da4f9a971fc473e93b30c073de9b8b37a3626baf
Łukasz Zawadzki <l.zawadzki@vp.pl>
2018-07-04 19:42:09 +0000

    first commit

--- src/CarsDataTable.js
+++ src/CarsDataTable.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import Car from "./Car";
+import Titles from "./Titles";
+
 
 const CarsDataTable = (props) => {
+    const allCars = props.cars.map((car, index) => (
+        <tr key={index}><Car carParams={car} /></tr>
+    ))
+const tableStyle = {border: "2px solid black"}
+const titles = Object.keys(props.cars[0]);
   return (
     <div>
       The list has {props.cars.length} cars.<br />
-      <b>Your task is to render a table here</b>.
+      <b>Your task is to render a table here </b>.
+        <table style={tableStyle}>
+            <Titles colNames={titles} />
+            {allCars}
+        </table>
     </div>
   );
 };
-
 export default CarsDataTable;
