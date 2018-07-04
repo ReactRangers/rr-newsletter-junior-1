b2fc4425544c1c1295819fc31deff6a1dc3ffc8f
Łukasz Zawadzki <l.zawadzki@vp.pl>
2018-07-04 20:47:05 +0000

    add styling

--- src/CarsDataTable.js
+++ src/CarsDataTable.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import Car from "./Car";
 import Titles from "./Titles";
-
+import "./CarsDataTable.css";
 
 const CarsDataTable = (props) => {
     const allCars = props.cars.map((car, index) => (
         <tr key={index}><Car carParams={car} /></tr>
     ))
-const tableStyle = {border: "2px solid black"}
-const titles = Object.keys(props.cars[0]);
+const tableStyle = {border: "2px solid grey", textAlign: "center", padding: 0}
+const divStyle = {textAlign: "center"}
   return (
-    <div>
+    <div style={divStyle} ClassName="table">
       The list has {props.cars.length} cars.<br />
       <b>Your task is to render a table here </b>.
         <table style={tableStyle}>
-            <Titles colNames={titles} />
+            <Titles />
             {allCars}
         </table>
     </div>
--- src/Titles.js
+++ src/Titles.js
@@ -3,8 +3,9 @@ import React, { Component } from "react";
 class Titles extends Component {
     render() {
         const titles = ["Make", "Model", "Model year", "Color", "Dealer e-mail"];
+        const tdStyle = {padding: "0 20px", backgroundColor: "#7f7f7f", fontWeight: "bold"}
         return(
-               titles.map(title => <td> {title} </td>)
+               titles.map(title => <td style={tdStyle}> {title} </td>)
             )
     }
 }
